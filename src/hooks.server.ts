6d29e587f926253655be57dbd076e52899f2bc52
amrkmn <amartukiminj@gmail.com>
2025-06-15 15:27:50 +0800

    fix: change log level from debug to info for ORIGIN logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,11 @@
+import { env } from '$env/dynamic/private';
 import { runMigrations } from '$lib/server/migrations';
+import { log } from '$lib/utils/logger';
 import {
-    deleteSessionTokenCookie,
-    invalidateSession,
-    setSessionTokenCookie,
-    validateSessionToken
+	deleteSessionTokenCookie,
+	invalidateSession,
+	setSessionTokenCookie,
+	validateSessionToken
 } from './lib/server/session';
 
 import type { Handle } from '@sveltejs/kit';
@@ -11,6 +13,7 @@ import type { Handle } from '@sveltejs/kit';
 await runMigrations();
 
 export const handle: Handle = async ({ event, resolve }) => {
+	log('env').info(`ORIGIN: ${env.ORIGIN}`);
 	const token = event.cookies.get('session') ?? null;
 	if (token === null) {
 		event.locals.user = null;
