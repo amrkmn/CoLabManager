289a2583991ac2f2c10b84a7aeafa228c1c2e783
amrkmn <amartukiminj@gmail.com>
2025-06-24 02:55:15 +0800

    refactor(auth): simplify load function by removing redirect

--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -1,13 +1,8 @@
 import { isFirstUserSetup } from '$lib/server/setup';
-import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ locals }) => {
 	const isFirstUser = await isFirstUserSetup();
-
-	if (!isFirstUser) {
-		throw redirect(302, '/auth/login');
-	}
 
 	return {
 		isFirstUser
